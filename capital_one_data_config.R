eb4c40f3cfebea857b96a768146bdfa64a9a2d0a
Alex Kiiru <kiiru7499@gmail.com>
2021-02-01 00:45:54 -0500

    Add files via upload

---
+++ capital_one_data_config.R
@@ -0,0 +1,60 @@
+
+# This file is for setting up all package needed for capital_data_challege
+
+#data processing and data transformation as well as for data visualization
+install.packages("tidyverse")
+
+#Package for generating reports 
+install.packages("data Explorer")
+
+#Package for tidyiing the data 
+install.packages("tidyr")
+
+#package used Used for importing data CSV files
+install.packages("readr")
+
+#package for creating pairwise plots 
+install.packages("GGally")
+
+#package displaying a table in HTML
+install.packages("DT")
+
+#Package used to add interactive maps 
+install.packages("leaflet")
+
+# package for Concatenating Name and Count in Graphs
+
+install.packages("glue")
+
+#package for preprocessing and Feature selection
+install.packages("caret")
+
+#interactive visualization 
+install.packages("plotly")
+
+#interactive visualization 
+install.packages("ggplotly")
+#Package For functions operating on rows and columns of matrices
+
+install.packages("matrixStats ")
+
+#package for manipualting rdata using sql
+install.packages("sqldf")
+
+#Package for Interactive Data Tables
+install.packages("kableExtra")
+
+#package for data visualition 
+install.packages("ggplot2")
+
+#package for using spreadsheet functions 
+install.packages("expss")
+
+##package ggmaps for visualizing map data 
+install.packages('ggmap')
+
+##install package prophet 
+install.packages('prophet')
+##install package mice
+install.packages('mice')
+
